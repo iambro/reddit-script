77114005a4c83a25274c098498a59602118161cd
Michal Ambrozewicz <michal.ambrozewicz@gmail.com>
2019-05-24 15:09:56 +0200

    Add properly date format

--- JS/index.js
+++ JS/index.js
@@ -5,24 +5,26 @@ let postsObject = {
   count: 0
 };
 
-function convertDate(x) {
-  const year = x.getFullYear();
-  const month = x.getMonth();
-  const day = x.getDay();
-  return `${year}-${month}-${day}`;
+function convertDate(el) {
+  const date = new Date();
+  const year = date.getFullYear(el);
+  const month = ("0" + (date.getMonth(el) + 1)).slice(-2);
+  const day = ("0" + date.getDay(el)).slice(-2);
+  const hh = ("0" + date.getHours(el)).slice(-2);
+  const mm = ("0" + date.getMinutes(el)).slice(-2);
+  return `${day}.${month}.${year} ${hh}:${mm}`;
 }
 
 fetch("https://www.reddit.com/r/funny.json")
   .then(response => response.json())
   .then(content => {
     content.data.children.map(post => {
-      const date = new Date(post.data.created);
       const element = {
         title: post.data.title,
         upvotes: post.data.ups,
         score: post.data.score,
         num_comments: post.data.num_comments,
-        created: convertDate(date)
+        created: convertDate(post.data.created)
       };
       postsObject.posts.push(element);
     });
