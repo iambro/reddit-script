7ca76de04cae014df60579c4327b5e74a1ace988
Michal Ambrozewicz <michal.ambrozewicz@gmail.com>
2019-05-24 19:46:40 +0200

    Add functionality of adding and showing posts

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,7 @@
-let postsList = {
+const postsList = {
   posts: [],
   count: 0
 };
-
-function convertDate(el) {
-  const date = new Date();
-  const year = date.getFullYear(el);
-  const month = ("0" + (date.getMonth(el) + 1)).slice(-2);
-  const day = ("0" + date.getDay(el)).slice(-2);
-  const hh = ("0" + date.getHours(el)).slice(-2);
-  const mm = ("0" + date.getMinutes(el)).slice(-2);
-  return `${day}.${month}.${year} ${hh}:${mm}`;
-}
 
 fetch("https://www.reddit.com/r/funny.json")
   .then(response => response.json())
@@ -29,16 +19,29 @@ fetch("https://www.reddit.com/r/funny.json")
     postsList.count = content.data.dist;
   });
 
-//   {
-//     "posts": [
-//         {
-//             "title": "put title here",
-//             "upvotes": 1234,
-//             "score": 1000,
-//             "num_comments": 100,
-//             "created": "16.05.2019 12:12",
-//         },
-//         ...
-//     ],
-//     "count": 10
-//     }
+function convertDate(el) {
+  const date = new Date();
+  const year = date.getFullYear(el);
+  const month = ("0" + (date.getMonth(el) + 1)).slice(-2);
+  const day = ("0" + date.getDay(el)).slice(-2);
+  const hh = ("0" + date.getHours(el)).slice(-2);
+  const mm = ("0" + date.getMinutes(el)).slice(-2);
+  return `${day}.${month}.${year} ${hh}:${mm}`;
+}
+
+function createPost(post) {
+  const tr = document.createElement("tr");
+  Object.keys(post).map(item => {
+    const content = document.createTextNode(post[item]);
+    const td = document.createElement("td");
+    td.appendChild(content);
+    tr.appendChild(td);
+  });
+  document.getElementById("tableBody").appendChild(tr);
+}
+
+function showList() {
+  postsList.posts.map(post => {
+    createPost(post);
+  });
+}
