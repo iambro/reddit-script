9cc8a5a0871d6b7a428cf5ce6d9686bbfba45f57
Michal Ambrozewicz <michal.ambrozewicz@gmail.com>
2019-05-24 21:17:36 +0200

    Add listeners and create handlers to inputs and button

--- JS/index.js
+++ JS/index.js
@@ -3,6 +3,10 @@ const postsList = {
   count: 0
 };
 
+let lastDay = false;
+let sortBy = "";
+let ascendingOrder = true;
+
 fetch("https://www.reddit.com/r/funny.json")
   .then(response => response.json())
   .then(content => {
@@ -17,10 +21,11 @@ fetch("https://www.reddit.com/r/funny.json")
       postsList.posts.push(element);
     });
     postsList.count = content.data.dist;
+    showList(postsList);
   });
 
 function convertDate(el) {
-  const date = new Date(el*1000);
+  const date = new Date(el * 1000);
   const year = date.getFullYear();
   const month = ("0" + (date.getMonth() + 1)).slice(-2);
   const day = ("0" + date.getDate()).slice(-2);
@@ -40,8 +45,39 @@ function createPost(post) {
   document.getElementById("tableBody").appendChild(tr);
 }
 
-function showList() {
-  postsList.posts.map(post => {
+function deleteList() {
+  const tableBody = document.getElementById("tableBody");
+  while (tableBody.firstChild) {
+    tableBody.removeChild(tableBody.firstChild);
+  }
+}
+
+function showList(list) {
+  list.posts.map(post => {
     createPost(post);
   });
 }
+
+function sort(e) {
+  postsList.posts.filter(post => console.log(post.score));
+}
+
+function filter() {
+  lastDay = !lastDay;
+
+  if (lastDay) {
+    deleteList();
+    showList(dupa);
+  } else {
+    deleteList();
+    showList(postsList);
+  }
+}
+
+function order() {
+  console.log("order");
+}
+
+document.getElementById("filter-input").addEventListener("change", filter);
+document.getElementById("sort-input").addEventListener("change", sort);
+document.getElementById("order-btn").addEventListener("click", order);
